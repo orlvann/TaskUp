bcb748a073e6326ec27fbee2affd4aecbb086259
orlvann <orlovaanna641@gmail.com>
2024-05-24 22:30:54 +0200

    the whiteboard and taskboard

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+// import AddBord from "../Components/AddBord";
+// import BordList from "../Components/BordList";
+import "./App.css";
+import AddBord from "../Components/ProjectManageBoard/AddBord";
+import BordList from "../Components/ProjectManageBoard/BordList";
+
+function App() {
+  const alldata = useSelector((state) => state);
+  useEffect(() => {
+    localStorage.setItem("manageState", JSON.stringify(alldata));
+  }, [alldata]);
+
+  return (
+    <div className="App bg-gradient-to-r from-cyan-900 to-indigo-900 w-full min-h-screen">
+      
+      {/* main board form/ */}
+      <AddBord />
+    {/* //All board list// */}
+      <BordList />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/WhiteBoard/WhiteBoard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Nav from '../Share/Nav';
+import { Tldraw } from 'tldraw';
+
+
+const WhiteBoard = () => {
+
+    return (
+        <div>
+            <Nav/>
+
+            {/* WhiteBoard? */}      
+			<div className="fixed inset-0 md:mt-20 mt-14"> 
+			    <Tldraw />
+		    </div>
+		
+           
+        </div>
+    );
+};
+
+export default WhiteBoard;
